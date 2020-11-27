6bd658ac8f65cc4927e79dca3f557cd707de2bb2
JMGK <jmgk77@gmail.com>
2020-11-27 16:17:48 -0300

    data conversion

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo *****************************************BUILD
-make
-echo *****************************************SEND CODE
-ps3load BARULANDIA.PS3.self
-echo *****************************************DEBUG
-netcat -l 18194
